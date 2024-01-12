71d61a8ff8887a36307025556c18908c2866a651
Yuliia-Vogel <arwen.vogel@gmail.com>
2024-01-12 14:18:45 +0200

    Odd cycles removed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from datetime import date, datetime, timedelta
 
-
+ 
 def get_birthdays_per_week(users):
     list_of_dates_in_week = []
     Monday = []
@@ -30,45 +30,42 @@ def get_birthdays_per_week(users):
     start_day_date = date_today + timedelta(start_day)
          
     for person in users:
-        for value in person.values():
-            if isinstance(value, date):
-                BD_replaced_year = value.replace(year=start_day_date.year)
-                one_week_later = start_day_date + timedelta(days=7)
-                if start_day_date <= BD_replaced_year <= one_week_later:
-                    print(f"{value} - a birthday comes this week")
-                    bd_name = person["name"]
-                    for i in list_of_dates_in_week:
-                        if BD_replaced_year == i:
-                            weekday_this_day = BD_replaced_year.weekday()
-                            if weekday_this_day == 0:
-                                Monday.append(bd_name)
-                                result["Monday"] = Monday
-                            elif weekday_this_day == 1:
-                                Tuesday.append(bd_name)
-                                result["Tuesday"] = Tuesday
-                            elif weekday_this_day == 2:
-                                Wednesday.append(bd_name)
-                                result["Wednesday"] = Wednesday
-                            elif weekday_this_day == 3:
-                                Thursday.append(bd_name)
-                                result["Thursday"] = Thursday
-                            elif weekday_this_day == 4:
-                                Friday.append(bd_name)
-                                result["Friday"] = Friday
-                            elif weekday_this_day == 5:
-                                Monday.append(bd_name)
-                                result["Monday"] = Monday
-                            elif weekday_this_day == 6:
-                                Monday.append(bd_name)
-                                result["Monday"] = Monday
-                            else:
-                                print("smth went wrong")
-                        else:
-                            continue
+        date_birthday = person["birthday"]
+        BD_replaced_year = date_birthday.replace(year=start_day_date.year)
+        one_week_later = start_day_date + timedelta(days=7)
+        if start_day_date <= BD_replaced_year <= one_week_later:
+            print(f"{date_birthday} - a birthday comes this week")
+            bd_name = person["name"]
+            for i in list_of_dates_in_week:
+                if BD_replaced_year == i:
+                    weekday_this_day = BD_replaced_year.weekday()
+                    if weekday_this_day == 0:
+                        Monday.append(bd_name)
+                        result["Monday"] = Monday
+                    elif weekday_this_day == 1:
+                        Tuesday.append(bd_name)
+                        result["Tuesday"] = Tuesday
+                    elif weekday_this_day == 2:
+                        Wednesday.append(bd_name)
+                        result["Wednesday"] = Wednesday
+                    elif weekday_this_day == 3:
+                        Thursday.append(bd_name)
+                        result["Thursday"] = Thursday
+                    elif weekday_this_day == 4:
+                        Friday.append(bd_name)
+                        result["Friday"] = Friday
+                    elif weekday_this_day == 5:
+                        Monday.append(bd_name)
+                        result["Monday"] = Monday
+                    elif weekday_this_day == 6:
+                        Monday.append(bd_name)
+                        result["Monday"] = Monday
+                    else:
+                        print("smth went wrong")
                 else:
-                    print("BD not this week")
-            else:
-                pass
+                    continue
+        else:
+            print("BD not this week")
     return result
 
 
